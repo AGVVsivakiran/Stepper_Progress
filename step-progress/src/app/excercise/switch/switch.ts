b900ddb8eeab5275db1b1a3caf175c04a84f03fc
AGVVsivakiran <sivakiran.adari@gmail.com>
2025-10-12 23:33:17 +0530

    Limited screen

--- step-progress/src/app/excercise/switch/switch.ts
+++ step-progress/src/app/excercise/switch/switch.ts
@@ -12,12 +12,35 @@ constructor(private route:Router){}
   back(){
     this.route.navigate(['step-progress']);
   }
-  selected:string = '1';
+  selected:string = '1'; //selecting tabs
+  screen = 1;  //basic selecting screen
+  limitedscreen:number = 1; //limited screen
 
+  //selecting tabs
   select(box: string){
     this.selected = box;
     console.log(this.selected = box);
   }
 
-  
+  //basic selecting screen
+  backscr(){
+this.screen--;
+console.log(this.screen);
+  }
+  fdscr(){
+this.screen++;
+console.log(this.screen);
+  }
+
+  //limited screen
+  backlim(){
+    if(this.limitedscreen > 1){
+      this.limitedscreen--;
+    }
+  }
+  fdrlim(){
+    if(this.limitedscreen < 5){
+      this.limitedscreen++;
+    }
+  }
 }
