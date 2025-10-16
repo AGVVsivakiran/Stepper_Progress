53cccd8f7d373c97738527549152d080486d90bd
AGVVsivakiran <sivakiran.adari@gmail.com>
2025-10-17 02:10:27 +0530

    Pop-Up and box

--- step-progress/src/app/excercise/switch/switch.ts
+++ step-progress/src/app/excercise/switch/switch.ts
@@ -15,6 +15,8 @@ constructor(private route:Router){}
   selected:string = '1'; //selecting tabs
   screen = 1;  //basic selecting screen
   limitedscreen:number = 1; //limited screen
+  boxclose = false; //for closing box
+  popup:number | null = null; //for popup box
 
   //selecting tabs
   select(box: string){
@@ -46,5 +48,17 @@ console.log(this.screen);
 
   limitsubmit(){
     alert("Form Submitted Successfully");
+  }
+
+  //for closing box
+  popupchanges(boxNumber?: number) {
+    if (boxNumber) {
+      // when clicking box-1 or box-2 buttons
+      this.popup = boxNumber;
+      this.boxclose = false;  // close popup
+    } else {
+      // toggle popup open/close when clicking main button or close icon
+      this.boxclose = !this.boxclose;
+    }
   }
 }
